58c392f8063f65f9b96905c2d1cd1cc49dd9bc89
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-01-30 17:35:28 +0100

    finished

--- index.js
+++ index.js
@@ -6,18 +6,18 @@
         X 25) Print in the console every string from the previous array
         X 26) Create an array with 100 random numbers in it
         X 27) Write a function to get the maximum and minimum values from the previously created array
-        28) Create an array of arrays, in which every array has 10 random numbers
+        X 28) Create an array of arrays, in which every array has 10 random numbers
         29) Create a function that gets 2 arrays as parameters and returns the longest one
         30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values
         */
-    //Ex 21   
+    //Ex 21  Given variables x = "John" and y = "Doe", write on the console "John <> Doe" 
        let x = "John <";
        let y = " > Doe";
 console.log(x+y);
-// for sure this isn't what was wanted for 22
+// for sure this isn't what was wanted for 
 
 
-//Ex 22
+//Ex 22 Create an object with properties such name, surname, email
 let contactCard = {
 firstName: "John",
 surName: "Smith",
@@ -27,26 +27,27 @@ email: "jonsmith@hotmail.com",
 console.log(contactCard);
 
 
-//Ex 23
+//Ex 23 Delete the email property from the previously created object
 delete contactCard.email;
 console.log(contactCard);
 
 
-//Ex 24
+//Ex 24 Create an array with 10 strings in it & 25 Print in the console every string from the previous array
 let myArr =["Frank","Nuts","Eat","P","New","QE","you","PU","ou","zz"];
 for (let i = 0; i < myArr.length; i++){
         console.log((i + 1) + '. ' + myArr[i])
     }
 
 
- //Ex25   
+ //Ex26   Create an array with 100 random numbers in it 
 const Marray = Array(100) 
 				.fill()
 				.map(() => Math.floor(100 * Math.random())); 
 
 console.log(Marray);
 
-// Ex 26
+
+// Ex 27  Write a function to get the maximum and minimum values from the previously created array
 let max = Marray[0], min = Marray[0];
 for (let i = 0; i < Marray.length; i++) {
 
@@ -58,11 +59,30 @@ console.log("max = " + max);
 console.log("min = " + min);
 
 
-//EX 27
-const Marray = Array(100) 
-				.fill()
-				.map(() => Math.floor(100 * Math.random())); 
 
-console.log(Marray);
 
-//Ex 28
+//Ex 28  Create an array of arrays, in which every array has 10 random numbers
+const crazyArr = [[10],[10],[10]]
+.fill()
+.map(() => 10 * Math.floor(10 * Math.random())); 
+
+console.log(crazyArr);
+//I am getting the random arrays but not 10 values each
+
+//29) Create a function that gets 2 arrays as parameters and returns the longest one
+
+/*
+let array1 = [11,12,13,14,15,97];
+        let array2 = [21,22,23,24,25];
+
+let myFunk = function ()
+{
+        for (let i=0;i<array1.length;i++){
+        if(arrr1<array2)
+        return array2}
+        else{
+
+        }
+}
+*/
+//30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values
